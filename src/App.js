9eb90180d941dbb527ff1fc53e4cae8c6cbb497c
tanmay5192 <tanmaytrivedi5@gmail.com>
2024-01-16 14:32:43 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Search from './components/search';
+import { toBeChecked } from '@testing-library/jest-dom/matchers';
 
 function App() {
   return (
@@ -11,3 +12,4 @@ function App() {
 }
 
 export default App;
+// this is test toBeChecked
